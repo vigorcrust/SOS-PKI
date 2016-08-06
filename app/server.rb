b14089b7ab1a0fe1304e08a8d40e4d25b7640301
Daniel Martens <ddmartens@gmail.com>
2016-08-06 23:04:27 +0200

    Added basic sinatra server and added run script.

---
+++ app/server.rb
@@ -0,0 +1,5 @@
+require 'sinatra'
+
+set :bind, '0.0.0.0'
+set :server, 'thin'
+
---
+++ run_server.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+docker	run \
+	-it \
+	--rm \
+	--name sos-pki-server \
+	-p 4567:4567 \
+	-v "$PWD"/app:/usr/src/app \
+	-v "$PWD"/bundle:/usr/local/bundle \
+	-w /usr/src/app \
+	sos-pki-dev \
+	ruby server.rb
